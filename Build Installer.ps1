04560949ad2b9f2b654f7367473745b3e2885ec9
Pavel Perestoronin <eigenein@gmail.com>
2011-09-24 15:04:33 +0300

    initial installer

--- Build Installer.ps1
+++ Build Installer.ps1
@@ -1,26 +1,45 @@
 
 function Set-Version
 {
-    Param ([string]$fileName, [string]$version)
-    $tempFileName = $fileName + ".tmp"
-    Get-Content $fileName |
-        %{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $NewVersion } |
-        %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $NewFileVersion } > $tempFileName
-    # Move-Item $tempFileName $fileName -Force
+    Param ([string]$filePath, [string]$version)
+    $tempFilePath = $filePath + ".tmp"
+    Get-Content $filePath |
+        %{[regex]::replace($_, 'AssemblyVersion\("[0-9]+(\.([0-9]+)){1,3}"\)', "AssemblyVersion(""${version}"")")} |
+        %{[regex]::replace($_, 'AssemblyFileVersion\("[0-9]+(\.([0-9]+)){1,3}"\)', "AssemblyFileVersion(""${version}"")")} > $tempFilePath
+    Move-Item $tempFilePath $filePath -Force
 }
 
 function Get-Version
 {
-    
+    Param([string]$filePath)
+    return Get-Content $filePath | 
+        %{[regex]::matches($_, 'AssemblyVersion\("(?<version>[0-9]+(\.([0-9]+)){1,3})"\)')} | %{$_.Groups['version'].value}
 }
 
+Write-Output 'Updating Version ...'
+$currentVersion = Get-Version 'Skype Historian\Properties\AssemblyInfo.cs'
+Write-Output "Current Version ${currentVersion}"
+$currentVersionObject = New-Object Version $currentVersion
+$nextVersionObject = New-Object Version($currentVersionObject.Major, $currentVersionObject.Minor, ($currentVersionObject.Build + 1), $currentVersionObject.Revision)
+$nextVersion = $nextVersionObject.ToString()
+Write-Output "Updating to ${nextVersion}"
+Set-Version 'Skype Historian\Properties\AssemblyInfo.cs' $nextVersion
 Write-Output 'Building Skype Historian ...'
-'C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild' 'Skype Historian.sln' /t:Rebuild /p:Configuration=Release /p:PlatformToolset=v90
+C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild 'Skype Historian.sln' /t:Rebuild /p:Configuration=Release /p:PlatformToolset=v90
 if ($LastExitCode -ne 0)
 {
     Write-Error 'Build Failed.'
 }
 else
 {
-    Write-Output 'Build Succeeded.'
+    Write-Output 'Building Installer ...'
+    & 'C:\Program Files (x86)\NSIS\makensis' 'Skype Historian.nsi'
+    if ($LastExitCode -eq 0)
+    {
+        Move-Item '..\Installers\Skype Historian Setup.exe' "..\Installers\Skype Historian ${nextVersion} Setup.exe" -Force
+    }
+    else
+    {
+        Write-Error 'Installer Build Failed'
+    }
 }
--- Skype Historian.nsi
+++ Skype Historian.nsi
@@ -1,1 +1,32 @@
 # Skype Historian Installer Build Script
+!include "MUI.nsh"
+
+Name "Skype Historian"
+OutFile "..\Installers\Skype Historian Setup.exe"
+InstallDir "$LOCALAPPDATA\Skype Historian"
+RequestExecutionLevel user
+
+!insertmacro MUI_PAGE_WELCOME
+!insertmacro MUI_PAGE_DIRECTORY
+!insertmacro MUI_PAGE_INSTFILES
+!insertmacro MUI_PAGE_FINISH
+
+!insertmacro MUI_UNPAGE_WELCOME
+!insertmacro MUI_UNPAGE_CONFIRM
+!insertmacro MUI_UNPAGE_INSTFILES
+!insertmacro MUI_UNPAGE_FINISH
+
+!insertmacro MUI_LANGUAGE "English"
+
+Section
+
+SetOutPath $INSTDIR
+WriteUninstaller $INSTDIR\Uninstaller.exe
+
+SectionEnd
+
+Section "Uninstall"
+
+Delete $INSTDIR\Uninstaller.exe
+
+SectionEnd
