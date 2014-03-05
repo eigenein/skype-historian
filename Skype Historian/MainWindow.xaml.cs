fb4d9ce2c26bef61dc1c8b413e8487b9c1b216a9
Pavel Perestoronin <eigenein@gmail.com>
2014-03-05 04:11:47 +0300

    Remove Flattr button

--- Skype Historian/MainWindow.xaml.cs
+++ Skype Historian/MainWindow.xaml.cs
@@ -88,8 +88,6 @@ namespace SkypeHistorian
                 "MainWindowLeftBarFinishing");
             confirmCloseString = ResourceManager.GetString(
                 "MainWindowConfirmClose");
-            flattrImage.ToolTip = ResourceManager.GetString(
-                "MainWindowFlattr");
             websiteHyperlink.NavigateUri = new Uri(ResourceManager.GetString(
                 "MainWindowWebsite"));
             websiteHyperlink.ToolTip = websiteHyperlink.NavigateUri.ToString();
@@ -221,12 +219,6 @@ namespace SkypeHistorian
                 Process.Start(new ProcessStartInfo(
                     ResourceManager.GetString("MainWindowWebsite")));
             }
-        }
-
-        private void FlattrImageMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo(
-                "http://flattr.com/thing/404856/Skype-Historian"));
         }
     }
 }   
